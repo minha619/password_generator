5d2bfd13432b567be9bd4a59a367c441dd3cd660
Your Name <minhakim@mins-mbp.home>
2021-09-12 21:01:17 -0400

    randomNumber function added

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,31 @@
 // Assignment code here
 
-function generatePassword(){
-    var str = 'abcdefghijklmnopqrstuvwxyz'
-    var num = '1234567890'
+var randomNumber = function (min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return value;
+};
+
+function generatePassword(charLength, addNum, addLower, addUpper, addSchar){
+    var str = "abcdefghijklmnopqrstuvwxyz"
+    var num = "1234567890"
     var sChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"
-
-
-    };
-  
-
+    var pw = ""
+    var pwtypes = []
+    
+    if(addNum === true){
+      pwtypes.push("number")
+    }
+    if(addLower === true){
+      pwtypes.push("lower")
+    }
+    if(addUpper === true){
+      pwtypes.push("upper")
+    }
+    if(addSchar === true){
+      pwtypes.push("special")
+    }
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); 
@@ -17,19 +34,19 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var charLength = window.prompt("How many characters would you like your password to contain?");
   if (charLength < 8){
-    window.alert ("You need to choose more than 8 characters");
+    window.alert ("Choose more than 8 characters");
     return
   }  else if (charLength > 128) {
-    window.alert("You need to choose less than 128 characters");
+    window.alert("Choose less than 128 characters");
     return
   }
-  var addNum = window.confirm("Click OK to confirm including special characters.");
-  var addLower = window.confirm("Click OK to confirm including numeric characters.");
-  var addUpper = window.confirm("Click OK to confirm including lowercase characters.");
-  var addSchar = window.confirm("Click OK to confirm including uppercase characters.");
-  
+  var addNum = window.confirm("Click OK to confirm including numeric characters.");
+  var addLower = window.confirm("Click OK to confirm including lowercase characters.");
+  var addUpper = window.confirm("Click OK to confirm including uppercase characters.");
+  var addSchar = window.confirm("Click OK to confirm including special characters.");
+
   if (addNum === false, addLower === false, addUpper === false, addSchar === false){
-    window.alert("You need to choose at least one special charater or numeric character or lowercase letter or uppercase letter.")
+    window.alert("You need to choose at least one option.")
     return
   }
 
